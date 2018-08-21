50c34cad0d07195c8ad45b789d47d36cd541dc6e
Maor Oz <maoroz92@gmail.com>
2018-08-22 01:40:06 +0300

    first commit

---
+++ src/containers/About.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { Head } from 'react-static';
+//
+
+export default () => (
+  <div>
+    <Head>
+      <title>February 30th - About</title>
+    </Head>
+  </div>
+)
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { withSiteData } from 'react-static'
+import { Head } from 'react-static'
+//
+import logoImg from '../logo.png'
+
+export default withSiteData(() => (
+  <div>
+    <Head>
+      <title>February 30th - Home</title>
+    </Head>
+  </div>
+))
